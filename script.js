5f4989af28b0c76a50bcc5a570ead11216866731
Dinuka <dinukanh@gmail.com>
2023-04-27 15:13:14 +1000

    create gameboard function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//create gameboard iife
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const display = () => {
+        let boardHTML = "";
+        //in square an index from array is entered)
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div id="square-${index}">${square}</div>`
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+    }
+
+    return {
+        display,
+    }
+})();
 
 
 
